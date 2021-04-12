2299fa6ba35fd5d6b23bea27bfa5c6cd50318a2a
bayu <bayuferdiman1@gmail.com>
2021-04-12 13:58:03 +0700

    landingPage-Bg

--- front-end/src/components/NavBar.js
+++ front-end/src/components/NavBar.js
@@ -9,7 +9,9 @@ import {
   NavItem,
   NavLink,
   NavbarText,
+  Button,
 } from "reactstrap";
+import logo from "../assets/logo.png";
 
 class NavBar extends Component {
   state = {
@@ -26,9 +28,24 @@ class NavBar extends Component {
   render() {
     return (
       <div className={style.header}>
+        {/* <div className={style.setHeader}> */}
         <div>
-            
+          {/* <img src={logo} /> */}
+          <div className={style.logo}></div>
         </div>
+        <div>
+          <p>coffe from indonesia</p>
+        </div>
+        <div>
+          <Button>shop</Button>
+        </div>
+        <div>
+        <Button>search</Button>
+        </div>
+        <div>
+          <p>your order (0)</p>
+        </div>
+        {/* </div> */}
       </div>
     );
   }
--- front-end/src/pages/LandingPage.js
+++ front-end/src/pages/LandingPage.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
+import style from "./page.module.css"
 
 class LandingPage extends Component {
   state = {};
   render() {
-    return <div>
-        
-        Landing
-        </div>;
+    return (
+      <div>
+        <div className={style.landingPage} >
+        LandingPage
+        </div>
+        hahah
+      </div>
+    );
   }
 }
 
